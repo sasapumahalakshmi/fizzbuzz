01422ac67a90d0d6415713b5355e3b41b31a869f
sasapumahalakshmi <70257616+sasapumahalakshmi@users.noreply.github.com>
2022-03-14 21:48:17 +0530

    commit

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+r=int(input("Enter a number :"))
+def  fizzbuzz(r):
+	for i in range(1,r):
+		if(i % 3 == 0 and i % 5 == 0):
+			print(str(i) + "= fizzbuzz")
+		else:
+			if(i % 3 == 0):
+				print(str(i) + "= fizz")
+			else:
+				if (i % 5 == 0):
+					print(str(i) + "= buzz")
+				else:
+					print(str(i))
+fizzbuzz(r) 
